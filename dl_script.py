50f94e4d63fd20ada577deda25d5f25e453aba88
kimda90 <kimda90@gmail.com>
2022-05-17 13:20:57 -0300

    make format and codec optional arguments of dl_script

--- dl_script.py
+++ dl_script.py
@@ -50,12 +50,12 @@ def _connect_to_resolve():
         resolve = dvr_script.scriptapp("Resolve")
         if resolve is not None:
             break
-        print "Waiting for Resolve to start..."
+        print("Waiting for Resolve to start...")
         time.sleep(5)
         i += 1
     if resolve is None:
         raise RuntimeError("Could not connect to DaVinci Resolve. There may be a problem starting it, or you may be using the free version.")
-    print "wait a bit more for resolve to become responsible"
+    print("wait a bit more for resolve to become responsible")
     time.sleep(5)
     return resolve
 
@@ -65,28 +65,28 @@ def _load_project(resolve, project_name, folders):
     if folders:
         folders = folders.replace("\\", "/")
         for folder in folders.split("/"):
-            print "Opening folder:", folder
+            print("Opening folder:", folder)
             assert project_manager.OpenFolder(folder), "Cannot open folder."
-    print "Loading project:", project_name
+    print("Loading project:", project_name)
     assert project_manager.LoadProject(project_name), "Cannot load project."
     project = project_manager.GetCurrentProject()
-    print project
+    print(project)
     return project
 
 
 def _set_timeline(project, timeline_name):
-    for i in xrange(int(project.GetTimelineCount())):  # GetTimelineCount returns float...
+    for i in range(int(project.GetTimelineCount())):  # GetTimelineCount returns float...
         timeline = project.GetTimelineByIndex(i + 1)  # index starts from 1
-        # print timeline.GetName()
+        # print(timeline.GetName())
         if timeline.GetName() == timeline_name:
-            print "Setting current timeline to", timeline.GetName()
+            print( "Setting current timeline to", timeline.GetName())
             assert project.SetCurrentTimeline(timeline), "Cannot set timeline."
 
 
 def _setup_render_job(project, formatted_output_path, format_="", codec="", render_preset=""):
     assert project.DeleteAllRenderJobs(), "Cannot delete render jobs..."
 
-    print "Loading render preset [{}]".format(render_preset)
+    print("Loading render preset [{}]".format(render_preset))
     assert project.LoadRenderPreset(render_preset), "Cannot set render_preset [{}]".format(render_preset)
 
     render_settings = {
@@ -98,7 +98,7 @@ def _setup_render_job(project, formatted_output_path, format_="", codec="", rend
     }
     assert project.SetRenderSettings(render_settings), "Cannot set render settings..."
 
-    if format_ and codec:
+    if format_ and codec and not render_preset:
         assert project.SetCurrentRenderFormatAndCodec(format_, codec), "Cannot set render format/codec."
 
     assert project.AddRenderJob(), "Cannot add render job..."
@@ -107,10 +107,10 @@ def _setup_render_job(project, formatted_output_path, format_="", codec="", rend
 def _start_render(project):
     assert project.StartRendering(1)
     while project.IsRenderingInProgress():
-        print project.GetRenderJobStatus(1)
+        print(project.GetRenderJobStatus(1))
         # sys.stdout.flush()
         time.sleep(1)
-    print project.GetRenderJobStatus(1)
+    print(project.GetRenderJobStatus(1))
 
 
 if __name__ == '__main__':
